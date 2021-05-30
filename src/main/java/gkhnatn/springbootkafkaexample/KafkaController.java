d752442fe98a905dadd50b59a0ed4f1200e60af7
gokhanatan <atan.gokhan@gmail.com>
2021-05-30 22:06:01 +0300

    Created publish kafka message endpoint

---
+++ src/main/java/gkhnatn/springbootkafkaexample/KafkaController.java
@@ -0,0 +1,25 @@
+package gkhnatn.springbootkafkaexample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+    private final KafkaProducer producer;
+
+    @Autowired
+    public KafkaController(KafkaProducer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> PublishMessage(@RequestParam String message){
+        producer.writeMessage(message);
+
+        return ResponseEntity.ok("Message was sent...");
+    }
+}
